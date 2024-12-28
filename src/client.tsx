e96f2653b903f5e08c5d7e9ab75d7da49ce0649c
clumsy-ug <rutobe740@gmail.com>
2024-12-28 21:38:15 +0900

    useNavigate追加

--- src/client.tsx
+++ src/client.tsx
@@ -1,8 +1,22 @@
+import { Link, useNavigate } from "@tanstack/react-router";
+
 export const Hello = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Hello World</h1>
       <p>Click on the links above to see the code splitting in action.</p>
+      <Link to="/">Go to Home</Link>
+      <button
+        onClick={() =>
+          navigate({
+            to: "/",
+          })
+        }
+      >
+        Go to Lazy
+      </button>
     </div>
   );
 };
