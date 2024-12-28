13439f3fab5848d043795a5a37a7030b0557473e
clumsy-ug <rutobe740@gmail.com>
2024-12-28 23:09:27 +0900

    パス無しのルート

--- src/route.tsx
+++ src/route.tsx
@@ -1,31 +1,70 @@
+// import {
+//   createRootRoute,
+//   createRoute,
+//   createRouter,
+//   Outlet,
+// } from "@tanstack/react-router";
+// import { Hello } from "./client";
+// import { Layout } from "./components/Layout";
+
+// const rootRoute = createRootRoute({});
+
+// const indexRoute = createRoute({
+//   getParentRoute: () => rootRoute,
+//   path: "/hello",
+//   component: () => (
+//     <Layout>
+//       <Outlet />
+//     </Layout>
+//   ),
+// });
+
+// const helloRoute = createRoute({
+//   getParentRoute: () => indexRoute,
+//   path: "/foo",
+//   component: () => <Hello />,
+// });
+
+// const routeTree = rootRoute.addChildren([indexRoute.addChildren([helloRoute])]);
+
+// export const router = createRouter({ routeTree });
+
+// declare module "@tanstack/react-router" {
+//   interface Register {
+//     router: typeof router;
+//   }
+// }
+
+
+
+
 import {
   createRootRoute,
   createRoute,
   createRouter,
-  Outlet,
 } from "@tanstack/react-router";
-import { Hello } from "./client";
-import { Layout } from "./components/Layout";
-
 const rootRoute = createRootRoute({});
 
-const indexRoute = createRoute({
+const greetingRoute = createRoute({
   getParentRoute: () => rootRoute,
-  path: "/hello",
-  component: () => (
-    <Layout>
-      <Outlet />
-    </Layout>
-  ),
+  id: "greeting",
 });
 
 const helloRoute = createRoute({
-  getParentRoute: () => indexRoute,
-  path: "/foo",
-  component: () => <Hello />,
+  getParentRoute: () => greetingRoute,
+  path: "/hello",
+  component: () => <p>Hello</p>,
 });
 
-const routeTree = rootRoute.addChildren([indexRoute.addChildren([helloRoute])]);
+const byeRoute = createRoute({
+  getParentRoute: () => greetingRoute,
+  path: "/bye",
+  component: () => <p>Bye</p>,
+});
+
+const routeTree = rootRoute.addChildren([
+  greetingRoute.addChildren([helloRoute, byeRoute]),
+]);
 
 export const router = createRouter({ routeTree });
 
