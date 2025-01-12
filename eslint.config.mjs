8612595254b2bf15946adc88db74c1c4d0d8575b
clumsy-ug <rutobe740@gmail.com>
2025-01-13 03:15:09 +0900

    env読み込みok。safeqlの型推論はなぜか効かない

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import safeql from "@ts-safeql/eslint-plugin/config";
+import tseslint from "typescript-eslint";
+import { DATABASE_URL } from './src/.env';
+
+export default tseslint.config(
+  ...tseslint.configs.recommended,
+  safeql.configs.connections({
+    databaseUrl: DATABASE_URL,
+    targets: [{ tag: "sql", transform: "{type}[]" }],
+  })
+);
