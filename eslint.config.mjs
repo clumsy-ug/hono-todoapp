e70cad2b647bfe1e164ce71f32df9522f9b56d14
clumsy-ug <rutobe740@gmail.com>
2025-01-13 23:51:05 +0900

    viteの環境変数取得方法を利用できた

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,10 @@
 import safeql from "@ts-safeql/eslint-plugin/config";
 import tseslint from "typescript-eslint";
-import { DATABASE_URL } from './src/env';
 
 export default tseslint.config(
   ...tseslint.configs.recommended,
   safeql.configs.connections({
-    databaseUrl: DATABASE_URL,
+    databaseUrl: import.meta.env.VITE_DATABASE_URL,
     targets: [{ tag: "sql", transform: "{type}[]" }],
   })
 );
--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,7 @@
 import postgres from "postgres";
-import { DATABASE_URL } from './env'
 
 export const main = async () => {
-  const sql = postgres(DATABASE_URL);
+  const sql = postgres(import.meta.env.VITE_DATABASE_URL);
 
   const result = await sql`SELECT id, name FROM users WHERE id = ${1}`;
   console.log("resultだよ", result);
