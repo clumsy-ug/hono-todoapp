5f5e7cfece038f009092450db177e9a7fc391884
clumsy-ug <rutobe740@gmail.com>
2025-03-02 06:15:33 +0900

    CORS対応

--- backend/src/index.tsx
+++ backend/src/index.tsx
@@ -1,9 +1,13 @@
 import { Hono } from 'hono'
 import { renderer } from './renderer'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
 app.use(renderer)
+
+// CORS should be called before the route
+app.use('/api/*', cors())
 
 app.get('/api', (c) => {
   return c.text('はじめる')
--- frontend/src/routes/-components/NavbarTop.tsx
+++ frontend/src/routes/-components/NavbarTop.tsx
@@ -2,6 +2,8 @@ import { Link } from "@tanstack/react-router";
 
 export default function NavbarTop() {
   return (
-    <Link to="/">ホーム</Link>
+    <div>
+      <Link to="/">ホーム</Link>
+    </div>
   )
 }
