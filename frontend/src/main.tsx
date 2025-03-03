72d1471f86193b4cbbf7b3cab71ef522f55a7e90
clumsy-ug <rutobe740@gmail.com>
2025-03-03 16:06:18 +0900

    queryセッティングok

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
 import { RouterProvider, createRouter } from "@tanstack/react-router"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import { routeTree } from "./routeTree.gen"
 
 // Create a new router instance
 const router = createRouter({ routeTree })
+
+// Create a client
+const queryClient = new QueryClient()
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
@@ -19,7 +24,10 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </StrictMode>
   )
 }
