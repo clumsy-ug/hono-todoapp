465112f418f27d9d3f31f853f9e43342ace5ea5d
clumsy-ug <rutobe740@gmail.com>
2025-03-04 13:27:11 +0900

    ~/ 使うには必要かも、メモ

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+// import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +12,9 @@ export default defineConfig({
   server: {
     port: 5000,
   },
+  // resolve: {
+  //   alias: {
+  //     '~': path.resolve(__dirname, '..')
+  //   }
+  // }
 })
