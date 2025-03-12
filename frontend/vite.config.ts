c4fb02b4e1e006e028be3f5847ddaec49bcf8080
clumsy-ug <rutobe740@gmail.com>
2025-03-12 14:56:47 +0900

    pathの ~ が効くようにした

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-// import path from 'path'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,9 +12,9 @@ export default defineConfig({
   server: {
     port: 5000,
   },
-  // resolve: {
-  //   alias: {
-  //     '~': path.resolve(__dirname, '..')
-  //   }
-  // }
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, '..')
+    }
+  }
 })
