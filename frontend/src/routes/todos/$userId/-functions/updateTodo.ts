089014c04a6448a94bdfeccb56806c5c73188448
clumsy-ug <rutobe740@gmail.com>
2025-04-05 20:13:30 +0900

    必要なエラーハンドリングを追加

--- frontend/src/routes/todos/$userId/-functions/updateTodo.ts
+++ frontend/src/routes/todos/$userId/-functions/updateTodo.ts
@@ -15,4 +15,10 @@ export const updateTodo = async({ user_id, content }: UpdateTodoProps): Promise<
   if (!res.ok) {
     throw new Error('TODO新規投稿に失敗しました')
   }
+
+  const result = await res.json()
+
+  if (result.status === 'failed') {
+    throw new Error(result.message)
+  }
 }
