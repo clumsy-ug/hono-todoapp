cae16ec88ce4a4c91e71618dae36acdd7d5464ef
clumsy-ug <rutobe740@gmail.com>
2025-04-27 14:08:38 +0900

    不要な確認用コード削除

--- frontend/src/routes/todos/$userId/-components/TodoContainer.tsx
+++ frontend/src/routes/todos/$userId/-components/TodoContainer.tsx
@@ -3,18 +3,10 @@ import { useGetTodos } from "../-functions/useGetTodos"
 import { TodoResponse } from "../-types"
 import TodoList from "./TodoList"
 import TodoForm from "./TodoForm"
-import useFetchTest from "../-functions/useFetchTest"
 
 export default function Todos() {
   const { userId } = useParams({ from: '/todos/$userId/' })
   const result: TodoResponse = useGetTodos(userId)
-
-  const credentials = {
-    mailAddress: "ここにemail",
-    password: "ここにpass"
-  }
-  const fetchTest = useFetchTest(credentials)
-  console.log("取れた", fetchTest)
 
   return (
     <div>
