dc07ba1228c0e63ae05924d6413bb3116048912e
clumsy-ug <rutobe740@gmail.com>
2025-05-04 11:24:11 +0900

    更新後、todoの順番が変わってしまう問題を解決

--- frontend/src/routes/todos/$userId/-functions/useUpdateTodo.ts
+++ frontend/src/routes/todos/$userId/-functions/useUpdateTodo.ts
@@ -2,16 +2,22 @@ import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { fetchUpdateTodo } from "./fetchUpdateTodo"
 import { todoKeys } from "../-key/key"
 import { UpdateTodoProps } from "~/backend/supabase/types"
+import { Todo } from "../-types"
 
 export const useUpdateTodo = () => {
   const queryClient = useQueryClient()
 
   const { mutate } = useMutation({
     mutationFn: (updateProps: UpdateTodoProps) => fetchUpdateTodo(updateProps),
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: todoKeys.all
-      })
+    onSuccess: (_, variables) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(todoKeys.all)
+      if (!previousTodos) return
+
+      const updatedTodos = previousTodos.map(todo => 
+        todo.id === variables.todoId ? { ...todo, content: variables.newContent } : todo
+      )
+
+      queryClient.setQueryData(todoKeys.all, updatedTodos)
 
       alert('更新成功')
     }
